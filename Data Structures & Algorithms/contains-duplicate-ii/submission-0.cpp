2b7e8933b2e983dae45bc7b31998a2c3fe03dd0a
Anusha Mehta <mehtanusha18@gmail.com>
2026-08-19 08:52:19 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+
+        for(int i=0;i<nums.size();i++){
+            int val = nums[i];
+
+            if(mp.find(val) != mp.end() && i-mp[val] <= k){
+                return true;
+            }
+            mp[val] = i;
+        }
+        return false;
+    }
+};
