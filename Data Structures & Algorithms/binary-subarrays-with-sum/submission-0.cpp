8df78d86e9ecd7a688d19285d822b7aa9881f5ac
Anusha Mehta <mehtanusha18@gmail.com>
2026-08-20 19:24:09 +0530

    Add: binary-subarrays-with-sum - submission-0

---
+++ Data Structures & Algorithms/binary-subarrays-with-sum/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+
+        unordered_map<int,int>mp;
+
+        int sum = 0;
+        int ans= 0;
+
+        mp[0] = 1;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(mp.find(sum - goal) != mp.end()){
+                ans += mp[sum-goal];
+            }
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
