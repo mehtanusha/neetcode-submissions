1071cf00717f876bef7963d2bf319386c6840fb6
Anusha Mehta <mehtanusha18@gmail.com>
2026-08-21 11:35:06 +0530

    Add: minimum-difference-between-highest-and-lowest-of-k-scores - submission-1

---
+++ Data Structures & Algorithms/minimum-difference-between-highest-and-lowest-of-k-scores/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j = k-1;
+        int ans = INT_MAX;
+        while(j<nums.size()){
+            ans = min(ans,nums[j] - nums[i]);
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};
