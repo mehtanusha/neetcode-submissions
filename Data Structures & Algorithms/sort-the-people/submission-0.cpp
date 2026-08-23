b07711d22700393d7720028dadc4865273164192
Anusha Mehta <mehtanusha18@gmail.com>
2026-08-24 00:11:25 +0530

    Add: sort-the-people - submission-0

---
+++ Data Structures & Algorithms/sort-the-people/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        unordered_map<int,string>mp;
+        int n = names.size();
+
+        for(int i=0;i<n;i++){
+            mp[heights[i]] = names[i];
+        }
+
+        sort(heights.rbegin(),heights.rend());
+        for(int i=0;i<n;i++){
+            names[i] = mp[heights[i]];
+        }
+        return names;
+    }
+};
