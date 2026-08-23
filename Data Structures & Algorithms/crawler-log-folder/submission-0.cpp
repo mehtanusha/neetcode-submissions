f87c22fb359f8dd6cc48ec2634c89f5927a11ffe
Anusha Mehta <mehtanusha18@gmail.com>
2026-08-23 23:23:37 +0530

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+     int depth = 0;
+     for(string &s : logs){
+        if(s == "../"){
+            if(depth > 0){
+                depth--;
+            }
+        }
+        else if(s == "./"){
+            continue;
+        }else{
+            depth++;
+        }
+     }   
+     return depth;
+    }
+};
