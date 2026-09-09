6fee1d2336f35147784e19188f92ccbf5712b14f
Anusha Mehta <mehtanusha18@gmail.com>
2026-09-09 23:22:14 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x < 2){
+            return x;
+        }
+        int left = 1;
+        int right = x/2;
+        int ans = 1;
+        
+        while(left <= right){
+            long long mid = left + (right - left)/2;
+
+            if(mid * mid == x) return mid;
+            else if(mid * mid < x){
+                ans = mid;
+                left = mid + 1;
+            }else{
+                right = mid -1;
+            }
+        }
+        return ans;
+    }
+};
